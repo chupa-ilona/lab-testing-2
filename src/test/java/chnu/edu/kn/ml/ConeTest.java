cf6cc67a35f32f09e5915a2fa15e0ea98d10fe00
Ilona Chupa <ilonachupa@gmail.com>
2026-02-23 16:53:05 +0200

    Update ConeTest.java
    made tests JUnit4

--- src/test/java/chnu/edu/kn/ml/ConeTest.java
+++ src/test/java/chnu/edu/kn/ml/ConeTest.java
@@ -1,41 +1,42 @@
 package chnu.edu.kn.ml;
 
-import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class ConeTests {
 
     @Test
-    void volume_shouldReturnCorrectValue() {
+    public void volume_shouldReturnCorrectValue() {
         Cone cone = new Cone(3, 4);
         double result = cone.volume();
         assertEquals(37.699, result, 0.01);
     }
 
     @Test
-    void getDiameter_shouldReturnCorrectValue() {
+    public void getDiameter_shouldReturnCorrectValue() {
         Cone cone = new Cone(5, 10);
         double result = cone.getDiameter();
-        assertEquals(10, result);
+        assertEquals(10, result, 0.01);
     }
 
     @Test
-    void getCircumference_shouldReturnCorrectValue() {
+    public void getCircumference_shouldReturnCorrectValue() {
         Cone cone = new Cone(10, 20);
         double result = cone.getCircumference();
         assertEquals(62.83, result, 0.01);
     }
 
     @Test
-    void baseArea_shouldReturnCorrectValue() {
+    public void baseArea_shouldReturnCorrectValue() {
         Cone cone = new Cone(10, 20);
         double result = cone.baseArea();
         assertEquals(314.15, result, 0.01);
     }
 
     @Test
-    void surfaceArea_shouldReturnCorrectValue() {
+    public void surfaceArea_shouldReturnCorrectValue() {
         Cone cone = new Cone(10, 20);
         double result = cone.surfaceArea();
         assertEquals(942.47, result, 0.01);
