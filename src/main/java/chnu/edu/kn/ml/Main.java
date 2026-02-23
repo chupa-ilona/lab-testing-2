0b7024d11cd900e77d55816c2d438bcc75c37091
Ilona Chupa <ilonachupa@gmail.com>
2026-02-23 16:37:26 +0200

    Update Main.java

--- src/main/java/chnu/edu/kn/ml/Main.java
+++ src/main/java/chnu/edu/kn/ml/Main.java
@@ -1,22 +1,14 @@
 package chnu.edu.kn.ml;
 
 
-/*
-  @author   george
-  @project   Default (Template) Project
-  @class  ${NAME}
-  @version  1.0.0 
-  @since 19.09.25 - 10.06
-*/
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
-        Rectangle rectangle = new Rectangle(3, 4);
-        int area = rectangle.getArea();
-        System.out.println(area);
+        Cone cone = new Cone(5, 10);
+        System.out.println(cone.volume());
+        System.out.println(cone.surfaceArea());
+        System.out.println(cone.getDiameter());
+        System.out.println(cone.getCircumference());
+        System.out.println(cone.baseArea());
 
     }
 }
